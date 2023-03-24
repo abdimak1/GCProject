a08e9acad061db92e20d00104161e3fa7a8ded2f
abdimak1 <79455537+abdimak1@users.noreply.github.com>
2023-03-24 04:24:25 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/Login';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
