f74d1d3b5be448c0037a01946fc62f8f003ceb4c
abdimak1 <79455537+abdimak1@users.noreply.github.com>
2023-04-03 16:05:56 -0800

    Update PrivateRoutes.js
    token

--- src/config/context/PrivateRoutes.js
+++ src/config/context/PrivateRoutes.js
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from 'react-router-dom'
 
 const PrivateRoutes = () => {
-    let auth = {'token':true}
+    let auth = {'token':false}
     return(
         auth.token ? <Outlet/> : <Navigate to="/login"/>
     )
