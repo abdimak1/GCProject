6d3b5e11b46b4b2ea6ab158470add2bb17ce7234
abdimak1 <79455537+abdimak1@users.noreply.github.com>
2023-05-28 18:50:38 -0800

    dashboard startup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import SideBar from "./scenes/global/SideBar";
+import FullFeaturedCrudGrid from "./scenes/UsersPages/allUsers";
 import CreatekebeleUser from "./scenes/UsersPages/Kebeleusers/CreateKebeleUser";
 import CreateregionalUser from "./scenes/UsersPages/Regionalusers/CreateRegionalUser";
 import CreateworedaUser from "./scenes/UsersPages/WoredaUsers/CreateWoredaUser";
@@ -12,15 +13,13 @@ import { ColorModeContext, useMode } from "./theme";
 import Regionalusers from "./scenes/UsersPages/Regionalusers";
 import WoredaUsers from "./scenes/UsersPages/WoredaUsers";
 import ZonalUsers from "./scenes/UsersPages/ZonalUsers";
+import Formuser from "./scenes/newuser";
 import Login from "./scenes/Login";
 import PrivateRoutes from "./config/context/PrivateRoutes";
 import AuthContext from "./config/context/authContext";
-import Dashboard from "@mui/icons-material/Dashboard";
 import { useLocation } from "react-router-dom";
 import MyResources from "./scenes/MyResources";
-import UpdateregionalUser from "./scenes/UsersPages/Regionalusers/updateUser";
-import UpdatezonalUser from "./scenes/UsersPages/ZonalUsers/updateUser";
-import UpdatekebeleadminUser from "./scenes/UsersPages/Kebeleusers/updateUser";
+import Dashboard from "./scenes/Dashboard";
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -50,18 +49,16 @@ function App() {
             <Routes>
               <Route element={<Login />} path="/" />
               <Route element={<PrivateRoutes />}>
-                <Route path="/dashboard" element={<Dashboard />} />
+                
+                <Route path="/dashboard" element={<Dashboard />}/> 
+                <Route path="/users" element={<FullFeaturedCrudGrid />} />
                 <Route path="/kebeleUsers" element={<KebeleUsers />} />
                 <Route path="/regional" element={<Regionalusers />} />
                 <Route path="/woreda" element={<WoredaUsers />} />
                 <Route path="/zonal" element={<ZonalUsers />} />
-                <Route path="/updateuser/:id" element={<UpdateregionalUser/>}/>
-                <Route path="/updatezoneuser/:id" element={<UpdatezonalUser/>}/>
-                <Route path="/updatekebeleuser/:id" element={<UpdatekebeleadminUser/>}/>
-
                 <Route
                   path="/createkebeleaccount"
-                  element={<CreatekebeleUser />}
+                  element={<CreatekebeleUser></CreatekebeleUser> }
                 />
                 <Route
                   path="/createregionalaccount"
@@ -75,6 +72,7 @@ function App() {
                   path="/createzonalaccount"
                   element={<CreatezoneUser />}
                 />
+                <Route path="/manageusers" element={<Formuser />} />
                 <Route path="/resources" element={<MyResources />} />
                 <Route path="*" element={<div>Page not Found!!</div>} />
               </Route>
